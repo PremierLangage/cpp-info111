e28c283e8df7dc3d84f17464455b8c9d9d63e189
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-16 23:47:55 +0200

    Fix

--- template/wims_output.pl
+++ template/wims_output.pl
@@ -26,9 +26,9 @@ def code_randomizer():
     d={}
     d['X'],d['Y'],d['Z'] = random.sample('XYZ',3)
     d['I'],d['J'],d['K'],d['N'] = random.sample('IJKN',4)
-    d['CI1'] = random.randint(-2,2)
-    d['CI2'] = random.randint(-2,2)
-    d['CI3'] = random.randint(-2,2)
+    d['CI1'] = str(random.randint(-2,2))
+    d['CI2'] = str(random.randint(-2,2))
+    d['CI3'] = str(random.randint(-2,2))
     def randomize_code(code):
         return re.sub(r'\b' + '|'.join(d.keys()) + r'\b',
                      lambda i: d[i.group()],
