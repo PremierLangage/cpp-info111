c9e9fc3cda70eedc4539a658c6a22bb2a6916a27
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-16 23:29:01 +0200

    Self-contained build

--- template/utils.py
+++
@@ -1,8 +0,0 @@
-import random, re
-def code_randomiser():
-    d={}
-    d['X'],d['Y'],d['Z'] = random.sample('XYZ',3)
-    d['I'],d['J'],d['K'],d['N'] = random.sample('IJKN',4)
-    def randomize_code(code):
-        return re.sub('|'.join(d.keys()), lambda i: d[i.group()], code)
-    return randomize_code()
--- template/wims_output.pl
+++ template/wims_output.pl
@@ -21,7 +21,16 @@ qu'il affiche.
 
 before==
 title=title+": "+topic
-import utils
+
+import random, re
+def code_randomiser():
+    d={}
+    d['X'],d['Y'],d['Z'] = random.sample('XYZ',3)
+    d['I'],d['J'],d['K'],d['N'] = random.sample('IJKN',4)
+    def randomize_code(code):
+        return re.sub('|'.join(d.keys()), lambda i: d[i.group()], code)
+    return randomize_code()
+
 code = utils.code_randomizer()(code)
 
 ==
