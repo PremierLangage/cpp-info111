4193ff989b7040bb8c7398a2e482a6446999cf39
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-16 23:39:28 +0200

    Typo

--- template/wims_output.pl
+++ template/wims_output.pl
@@ -27,7 +27,7 @@ def code_randomizer():
     d['X'],d['Y'],d['Z'] = random.sample('XYZ',3)
     d['I'],d['J'],d['K'],d['N'] = random.sample('IJKN',4)
     def randomize_code(code):
-        return re.sub(r'\b' + ''|'.join(d.keys()) + r'\b',
+        return re.sub(r'\b' + '|'.join(d.keys()) + r'\b',
                      lambda i: d[i.group()],
                      code)
     return randomize_code
