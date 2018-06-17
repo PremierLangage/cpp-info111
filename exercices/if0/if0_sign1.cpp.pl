97236fed25e539f4855fda8ef75c2d05db8810e5
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-17 07:54:34 +0200

    Added HIDDEN markers

--- exercices/if0/if0_sign1.cpp.pl
+++ exercices/if0/if0_sign1.cpp.pl
@@ -1,16 +1,20 @@
 template=/template/wims_output.pl
 topic=if0
 code==
+/// BEGIN HIDDEN
 #include <iostream>
 using namespace std;
 
 int main() {
+    /// END HIDDEN
     int X = CI1;
     if ( X < 0 ) {
         cout << "negatif" << endl;
     } else {
         cout << "positif" << endl;
     }
+    /// BEGIN HIDDEN
     return 0;
 }
+/// END HIDDEN
 ==
