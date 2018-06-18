ad853c579e57da8c0f81d05b7486d291f93f112b
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-18 09:31:31 +0200

    Experiment with externalizing the source code

--- exercices/io/basic_1.cpp.pl
+++ exercices/io/basic_1.cpp.pl
@@ -1,19 +1,3 @@
 template=/template/wims_input.pl
 topic=io
-code==
-/// BEGIN HIDDEN
-#include "randomization.h"
-#include <iostream>
-using namespace std;
-
-int main() {
-    /// END HIDDEN
-    int X;
-
-    cin >> X;
-    cout << X + CI1 << endl;
-    /// BEGIN HIDDEN
-    return 0;
-}
-/// END HIDDEN
-==
+code=@/exercices-sources/io/basic_1.cpp
