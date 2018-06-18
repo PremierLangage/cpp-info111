cd61dab2c97091a1eaabfabaed0dc41c7c5c023e
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-06-18 09:42:49 +0200

    Added test file

---
+++ template/test.cpp
@@ -0,0 +1,15 @@
+/// BEGIN HIDDEN
+#include "randomization.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    /// END HIDDEN
+    int X;
+
+    cin >> X;
+    cout << X + CI1 << endl;
+    /// BEGIN HIDDEN
+    return 0;
+}
+/// END HIDDEN
