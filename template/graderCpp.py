5612538bd397495b355652d1817f81c92a480975
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-07-04 14:21:14 +0200

    Renaming err_out -> std_err for consistency

--- template/graderCpp.py
+++ template/graderCpp.py
@@ -8,20 +8,20 @@ def compile_and_run(code, input=""):
         >>> import graderCpp
         >>> graderCpp.compile_and_run('it main() {}') == {
         ...    'compile_err': 256,
-        ...    'compile_err_out': 'code.cpp:1:1: error: ‘it’ does not name a type; did you mean ‘int’?\n it main() {}\n ^~\n int\n',
+        ...    'compile_std_err': 'code.cpp:1:1: error: ‘it’ does not name a type; did you mean ‘int’?\n it main() {}\n ^~\n int\n',
         ...    'compile_std_out': ''}
         True
 
         >>> graderCpp.compile_and_run('int main() {}') == {
         ...     'compile_err': 0,
-        ...     'compile_err_out': '',
+        ...     'compile_std_err': '',
         ...     'compile_std_out': '',
         ...     'err': 0,
-        ...     'err_out': '',
+        ...     'std_err': '',
         ...     'std_out': ''}
         True
 
-        >>> graderCpp.compile_and_run('#include<iostream>\n int main() { std::cout << "coucou" << std::endl;}') == {'compile_err': 0, 'compile_err_out': '', 'compile_std_out': '', 'err': 0, 'err_out': '', 'std_out': 'coucou\n'}
+        >>> graderCpp.compile_and_run('#include<iostream>\n int main() { std::cout << "coucou" << std::endl;}') == {'compile_err': 0, 'compile_std_err': '', 'compile_std_out': '', 'err': 0, 'std_err': '', 'std_out': 'coucou\n'}
         True
     """
     log = {}
@@ -29,13 +29,13 @@ def compile_and_run(code, input=""):
     gcc_cmd = "g++ -Wall -Wno-sign-compare -Wno-unused-value -pedantic -std=c++11 " + "code.cpp -o code"
     log['compile_err'] = os.system(gcc_cmd + "> compilCstdout.log 2> compilCstderr.log ")
     # Get back the standard/error output of compilation
-    log['compile_err_out'] = open("compilCstderr.log", "r").read()
+    log['compile_std_err'] = open("compilCstderr.log", "r").read()
     log['compile_std_out'] = open("compilCstdout.log", "r").read()
 
     if not log['compile_err']:
         io.open("stdin", "w").write(input)
         log['err'] = os.system("./code < stdin 2> stderr.log > stdout.log")
-        log['err_out'] = open("stderr.log", "r").read()
+        log['std_err'] = open("stderr.log", "r").read()
         log['std_out'] = open("stdout.log", "r").read()
     return log
 
@@ -60,10 +60,10 @@ def grader_expected_output(exo):
     log = compile_and_run(code=exo['code'], )
     response = { 'success': True, 'feedback':'Bonne réponse' }
     if log['compile_err']:
-        response['feedback'] = log['compile_err_out']
+        response['feedback'] = log['compile_std_err']
         return response
     if log['err']:
-        response['feedback'] = log['err_out']
+        response['feedback'] = log['std_err']
         return response
     if not equal_out(log['std_out'], answer):
         response['success'] = False
@@ -91,10 +91,10 @@ def grader_input(exo):
     log = compile_and_run(code=exo['code'], input=answer)
     response = { 'success': True, 'feedback':'Bonne réponse' }
     if log['compile_err']:
-        response['feedback'] = log['compile_err_out']
+        response['feedback'] = log['compile_std_err']
         return response
     if log['err']:
-        response['feedback'] = log['err_out']
+        response['feedback'] = log['std_err']
         return response
     if not equal_out(log['std_out'], "42"):
         response['success'] = False
