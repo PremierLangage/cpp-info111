06424a36e13a2c58b67c2f31791bf1b20e7bb11f
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-01-16 22:20:37 +0100

    Update to new randomization

--- exercices_src/test_input.cpp
+++ exercices_src/test_input.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 using namespace std;
 
+CONST C = RANDOM_INT(-2,2);
+
 int main() {
 /// END HIDDEN
     int X;
     cin >> X;
-    cout << X + CI1 << endl;
+    cout << X + C << endl;
 /// BEGIN HIDDEN
     return 0;
 }
