e8b79c20e92766a096dfc8299256480d7209bbf6
Florent Hivert <Florent.Hivert@lri.fr>
2019-02-20 17:01:10 +0100

    struct simple

--- 2019/struct_simple/struct_simple_1.cpp
+++ 2019/struct_simple/struct_simple_1.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+/// BEGIN HIDDEN
+#include "randomization.h"
+CONST V1 = RANDOM_VECTOR(3, RANDOM_INT, -5, 5);
+CONST V2 = RANDOM_VECTOR(3, RANDOM_INT, -5, 5);
+/// END HIDDEN
+
+
 struct Point3D {
     double x;
     double y;
@@ -10,8 +17,8 @@ struct Point3D {
 };
 
 int main() {
-    Point3D p1 = {5, -3, 2};
-    Point3D p2 = {8, 2, 6};
+    Point3D p1 = V1;
+    Point3D p2 = V2;
 
     double dx = fabs(p1.x - p2.x);
     double dy = fabs(p1.y - p2.y);
