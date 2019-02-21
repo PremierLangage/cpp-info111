439ddd053b40e864fbe94fe091e2dd4a85d47a2f
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-21 13:54:47 +0100

    procedure simples avec le make

---
+++ exercices/procedure_simplest/procedure_1_input.cpp.pl
@@ -0,0 +1,6 @@
+
+template=/template/wims_input.pl
+topicname=procédures simples
+@ /exercices_src/procedure_simplest/procedure_1_input.cpp [raw_code.cpp]
+# template=/exercises_src/procédures simples/metadata.pl
+code_file=raw_code.cpp
