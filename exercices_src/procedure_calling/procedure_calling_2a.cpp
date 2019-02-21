c1ec4d944d6ead11277a1359fb7902b647bec08e
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-21 16:33:34 +0100

    premier test de val ou ref

--- exercices_src/procedure_calling/procedure_calling_2a.cpp
+++ exercices_src/procedure_calling/procedure_calling_2a.cpp
@@ -2,20 +2,27 @@
 
 using namespace std;
 
-void g(int &i, int j) {
-    i = i + j;
+/// BEGIN HIDDEN
+#include "randomization.h"
+CONST I1 = RANDOM_INT(2, 5);
+CONST I2 = RANDOM_INT(2, 5);
+CONST I3 = RANDOM_INT(2, 5);
+/// END HIDDEN
+
+void g(int VALOUREF X, int VALOUREF Y) {
+    X = X + Y;
 }
 
-void f(int &i, int j) {
-    i = i + 3;
-    g(i, j);
+void f(int VALOUREF X, int VALOUREF Y) {
+    X = X + I1;
+    g(X, Y);
 }
 
 int main() {
-    int i = 0;
-    int j;
-    j = i + 5;
-    f(i, j);
-    cout << i << endl;
+    int X = I2;
+    int Y;
+    Y = X + I3;
+    f(X, Y);
+    cout << X << endl;
     return 0;
 }
