23d24dac036a74c34a7fcda9bdb51489f57d33cd
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-22 12:17:15 +0100

    maj de la liste des topics

---
+++ exercices/function_call_simple/function_call_1.cpp.pl
@@ -0,0 +1,5 @@
+
+template=/template/wims_output.pl
+topicname=appel de fonctions simple
+@ /exercices_src/function_call_simple/function_call_1.cpp [raw_code.cpp]
+code_file=raw_code.cpp
---
+++ exercices/function_call_simple/function_inversion_arguments.cpp.pl
@@ -0,0 +1,5 @@
+
+template=/template/wims_output.pl
+topicname=appel de fonctions simple
+@ /exercices_src/function_call_simple/function_inversion_arguments.cpp [raw_code.cpp]
+code_file=raw_code.cpp
