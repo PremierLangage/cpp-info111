8eba564e2bc6460d7397f46981b4b74529fc763f
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-24 15:38:21 +0100

    fix de procedure_calling_2_input car parfois impossible

--- exercices_src/procedure_calling/procedure_calling_2_input.cpp
+++ exercices_src/procedure_calling/procedure_calling_2_input.cpp
@@ -6,7 +6,6 @@ using namespace std;
 #include "randomization.h"
 CONST I1 = RANDOM_INT(2, 5);
 CONST I2 = RANDOM_INT(2, 5);
-CONST I3 = RANDOM_INT(2, 5);
 CONST V1 = RANDOM_VALOUREF();
 CONST V2 = RANDOM_VALOUREF();
 CONST V3 = RANDOM_VALOUREF();
@@ -15,7 +14,7 @@ CONST V4 = RANDOM_VALOUREF();
 
 
 void g(int V1 X, int V2 Y) {
-    X = X PLUSOUMOINS Y;
+    X = X + Y;
 }
 
 void f(int V3 X, int V4 Y) {
@@ -27,7 +26,7 @@ int main() {
     int X;
     int Y;
     cin >> X;
-    Y = X + I3;
+    Y = X + I2;
     f(X, Y);
     cout << X << endl;
     return 0;
