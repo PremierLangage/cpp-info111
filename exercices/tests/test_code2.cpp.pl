46c73c6fdcf1dca4f2a3bdda409a34bb54be312c
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-03-20 17:31:41 +0100

    Reimport_from_wims

--- exercices/tests/test_code2.cpp.pl
+++
@@ -1,5 +0,0 @@
-
-template=/template/wims_output.pl
-topicname=tests des fonctionalités de la plateforme
-@ /exercices_src/tests/test_code2.cpp [raw_code.cpp]
-code_file=raw_code.cpp
---
+++ exercices/tests/test_multisolution_code.cpp.pl
@@ -0,0 +1,5 @@
+
+template=/template/wims_code.pl
+topicname=tests des fonctionalités de la plateforme
+@ /exercices_src/tests/test_multisolution_code.cpp [raw_code.cpp]
+code_file=raw_code.cpp
