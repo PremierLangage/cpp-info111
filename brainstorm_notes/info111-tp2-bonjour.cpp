b91fe1709b1a36e420e60f3d110cd3b56621a42f
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-03-25 12:16:22 +0100

    Imported brainstorm notes

---
+++ brainstorm_notes/info111-tp2-bonjour.cpp
@@ -0,0 +1,24 @@
+/// BEGIN HIDDEN
+/// - keywords: orsay-mpi-info-111-tp2, variables
+/// - description:
+///   - fr:
+///   - en:
+#include<iostream>
+using namespace std;
+
+int main () {
+/// END HIDDEN
+    string prenom;
+    prenom = "Claude";
+    /// BEGIN SOLUTION
+    cout << "Bonjour, mon nom est " << prenom << endl;
+    cout << prenom << " est dans la classe" << endl;
+    cout << "Sans le moindre doute, " << prenom << " est dans la classe" << endl;
+    /// END SOLUTION
+    /// BEGIN XXX
+    ///...
+    /// END XXX
+/// BEGIN HIDDEN
+    return 0;
+}
+/// END HIDDEN
