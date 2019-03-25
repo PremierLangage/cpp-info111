58aa30dfb9e03d197e5e4671a2e52c5367925759
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-03-25 09:53:51 +0100

    Typo

--- exercices/tests/test_input.cpp.pl
+++ exercices/tests/test_input.cpp.pl
@@ -1,4 +1,4 @@
 template=/template/wims_input.pl
-template=/exercises_src/tests/metadata.pl
+template=/exercices_src/tests/metadata.pl
 @ /exercices_src/tests/test_input.cpp [raw_code.cpp]
 code_file=raw_code.cpp
