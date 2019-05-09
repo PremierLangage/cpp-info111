54fa3154f4fa0be4e4476d90673a07e196d005ca
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-05-09 16:08:43 +0200

    update function call

---
+++ exercices/function_call_simple/reverse_int_code.cpp.pl
@@ -0,0 +1,4 @@
+template=/template/wims_code.pl
+template=/exercices_src/function_call_simple/metadata.pl
+@ /exercices_src/function_call_simple/reverse_int_code.cpp [raw_code.cpp]
+code_file=raw_code.cpp
