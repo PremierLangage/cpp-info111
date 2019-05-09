1e6f5f6c2f0ae57019f5e5adb4a14bec07595e95
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-05-09 15:42:32 +0200

    update procedure simplest

---
+++ exercices/procedure_simplest/procedure_ref_val_1.cpp.pl
@@ -0,0 +1,4 @@
+template=/template/wims_output.pl
+template=/exercices_src/procedure_simplest/metadata.pl
+@ /exercices_src/procedure_simplest/procedure_ref_val_1.cpp [raw_code.cpp]
+code_file=raw_code.cpp
