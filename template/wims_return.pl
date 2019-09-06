8f93715c575447fc8a5c0036707d661dd1c34c09
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-09-06 08:54:54 +0200

    add new exercise type: return (WIP)

--- template/wims_return.pl
+++ template/wims_return.pl
@@ -1,10 +1,10 @@
 # Copyright 2019 Nicolas M. Thiéry ...
 
 # In an exercise of this type, the user is given a function of one
-# argument, an input value, and must guess the output value.
+# argument, an input value, and must guess the return value.
 #
 # Code requirements: the code should:
-# - define a function f and hide everything else
+# - define a function f of a single input and hide everything else
 # - define a randomized constant INPUT
 # - print the result of f(INPUT)
 #
@@ -18,5 +18,5 @@ type=sandbox
 failure_message=Réponse incorrecte:<pre>{answer_out}</pre>La fonction renvoie:<pre>{solution_out}</pre>
 
 text==
-Exécuter pas à pas la fonction suivante. Que renvoie <tt>f({consts['INPUT']})</tt>?
+Exécuter pas à pas la fonction suivante. Que renvoie <tt>f({{consts['INPUT']}})</tt>?
 ==
