b5fd94236ff27092827feb8a9d3d7b446d8b9f48
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-09-06 08:48:27 +0200

    add new exercise type: return (WIP)

--- exercices_src/tests/test_return.cpp
+++ exercices_src/tests/test_return.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include "randomization.h"
 using namespace std;
-CONST int THRESHOLD = RANDOM_INT(-1,1)
-CONST int RETURN_VALUE_TRUE = RANDOM_INT(4)
-CONST int RETURN_VALUE_FALSE = RANDOM_INT(4)
+
+CONST THRESHOLD = RANDOM_INT(-1,1)
+CONST RETURN_VALUE_TRUE = RANDOM_INT(4)
+CONST RETURN_VALUE_FALSE = RANDOM_INT(4)
 /// END HIDDEN
 int f(int X) {
     if ( X < THRESHOLD ) {
@@ -14,7 +15,7 @@ int f(int X) {
     }
 }
 /// BEGIN HIDDEN
-CONST int INPUT = RANDOM_INT(-3, 3);
+CONST INPUT = RANDOM_INT(-3, 3);
 int main() {
     cout << f(INPUT) << endl;
     return 0;
