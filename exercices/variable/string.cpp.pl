9533ddb7dfabd0e18066538880a26cacb5d42798
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-11 17:02:12 +0200

    make after new exercice

---
+++ exercices/variable/string.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_output.pl
+template=/cpp-info111/exercices_src/variable/metadata.pl
+@ /cpp-info111/exercices_src/variable/string.cpp [raw_code.cpp]
+code_file=raw_code.cpp
