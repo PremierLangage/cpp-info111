48e94da4f89ccca2cdb3c1ceb16d5675fdba5686
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-12 17:28:49 +0200

    reorder variables exercices

--- exercices/variable/g_echange.cpp.pl
+++ exercices/variable/g_echange.cpp.pl
@@ -1,4 +1,4 @@
 template=/cpp-info111/template/wims_output.pl
 template=/cpp-info111/exercices_src/variable/metadata.pl
-@ /cpp-info111/exercices_src/variable/f_echange_fail2.cpp [raw_code.cpp]
+@ /cpp-info111/exercices_src/variable/g_echange.cpp [raw_code.cpp]
 code_file=raw_code.cpp
