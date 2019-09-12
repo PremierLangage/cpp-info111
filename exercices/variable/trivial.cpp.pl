ef6bfd99de94fa13a04f91dcf0748351109d66d9
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-12 16:36:37 +0200

    update variable pltp

--- exercices/variable/trivial.cpp.pl
+++
@@ -1,4 +0,0 @@
-template=/cpp-info111/template/wims_output.pl
-template=/cpp-info111/exercices_src/variable/metadata.pl
-@ /cpp-info111/exercices_src/variable_trivial.cpp [raw_code.cpp]
-code_file=raw_code.cpp
--- exercices_src/variable_caractere.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-    char x = 'a';
-    cout << x << endl;
-    cout << 'x' << endl;
-}
