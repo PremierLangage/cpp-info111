80daec8c982462b735e798455f4bd84b43cce247
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-12 15:52:36 +0200

    make after new exercices

---
+++ exercices/variable/echange_code.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_code.pl
+template=/cpp-info111/exercices_src/variable/metadata.pl
+@ /cpp-info111/exercices_src/variable/echange_code.cpp [raw_code.cpp]
+code_file=raw_code.cpp
