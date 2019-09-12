9bd5ed475a8e961934a1924f597f5a4be7f9b6ff
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-12 17:00:58 +0200

    new pltp for arithmetic

---
+++ exercices/arithmetic/division_entiere.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_output.pl
+template=/cpp-info111/exercices_src/arithmetic/metadata.pl
+@ /cpp-info111/exercices_src/arithmetic/division_entiere.cpp [raw_code.cpp]
+code_file=raw_code.cpp
