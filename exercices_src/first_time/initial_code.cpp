a469fa8611cd2efea7201b46377ac9ab880db016
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-13 16:12:56 +0200

    new ex in decourverte

--- exercices_src/first_time/initial_code.cpp
+++ exercices_src/first_time/initial_code.cpp
@@ -7,10 +7,13 @@ CONST I1 = RANDOM_INT(1,10);
 
 int main() {
 /// END HIDDEN
+  /* Déclarez une variable entière r et affectez lui la valeur I1. */
+/// BEGIN SOLUTION
     int r = I1;
+/// END SOLUTION
 /// BEGIN HIDDEN
     cout << r << endl;
     return 0;
 }
-/// Objectif Pédagogique : prise en main de PL.
+/// Objectif Pédagogique : savoit déclarer une variable et prise en main PL_code.
 /// END HIDDEN
