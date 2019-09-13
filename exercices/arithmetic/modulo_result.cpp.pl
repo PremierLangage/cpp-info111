028854edf3b53b9d6af53ee9cbbc269b0cd026ef
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-09-13 16:25:19 +0200

    make after new exos

---
+++ exercices/arithmetic/modulo_result.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_result.pl
+template=/cpp-info111/exercices_src/arithmetic/metadata.pl
+@ /cpp-info111/exercices_src/arithmetic/modulo_result.cpp [raw_code.cpp]
+code_file=raw_code.cpp
