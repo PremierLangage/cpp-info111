c49bf3f293d15994024630cc6c8d82cdd50429c0
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-10-14 18:18:31 +0200

    make after modifications

---
+++ exercices/for/for_binomial_input.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_input.pl
+template=/cpp-info111/exercices_src/for/metadata.pl
+@ /cpp-info111/exercices_src/for/for_binomial_input.cpp [raw_code.cpp]
+code_file=raw_code.cpp
