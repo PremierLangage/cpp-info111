ba805f785b9cc9d53f6af539196bf20e81ff221a
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2019-10-15 15:31:20 +0200

    nthiéry modified Yggdrasil/cpp-info111/exercices_src/vector1D/recherche_first_decreasing.cpp

--- exercices_src/vector1D/recherche_first_decreasing.cpp
+++ exercices_src/vector1D/recherche_first_decreasing.cpp
@@ -1,8 +1,27 @@
+/// BEGIN HIDDEN
+#include <iostream>
+#include <vector>
+#include "randomization.h"
+using namespace std;
 
-CONST A  = RANDOM_INT(0, 4);
+CONST I1  = RANDOM_INT(0, 4);
+CONST TAB = RANDOM_VECTOR(RANDOM_INT(4, 7), RANDOM_INT, 0, 6);
+  
+/// END HIDDEN
+int recherche(int x, vector<int> t) {
+    int indice = -1;
+    for (int i = t.size() - 1; i >= 0; i = i - 1) {
+        if (t[i] == x) {
+            indice = i;
+        }
+    }
+    return indice;
+}
+
 
 int main() {
-    A;
+    vector<int> tab = TAB;
+    cout << recherche(I1, tab) << endl;
     return 0;
 }
 
