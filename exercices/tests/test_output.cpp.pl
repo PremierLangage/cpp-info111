d0941c150d7e66d49618b8a28072d5659eb1d0e3
Hugo Mlodecki <hugo.mlodecki@u-psud.fr>
2019-10-15 14:36:15 +0200

    hmlodecki modified Yggdrasil/cpp-info111/exercices/tests/test_output.cpp.pl

--- exercices/tests/test_output.cpp.pl
+++ exercices/tests/test_output.cpp.pl
@@ -1,4 +1,5 @@
 template=/cpp-info111/template/wims_output.pl
-template=/cpp-info111/exercices_src/tests/metadata.pl
+template=/cpp-info111/exercices_src/first_time/metadata.pl
 @ /cpp-info111/exercices_src/tests/test_output.cpp [raw_code.cpp]
 code_file=raw_code.cpp
+
