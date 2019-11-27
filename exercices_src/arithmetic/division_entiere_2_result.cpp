d48ddb8afbfb01506b00936e8ed887234071c5a8
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-11-27 14:40:42 +0100

    update arithmetic

--- exercices_src/arithmetic/division_entiere_2_result.cpp
+++ exercices_src/arithmetic/division_entiere_2_result.cpp
@@ -1,1 +1,17 @@
-division_entiere_result.cpp
+/// BEGIN HIDDEN
+#include <iostream>
+#include "randomization.h"
+using namespace std;
+
+CONST I1 = RANDOM_CHOICE(3, 5, 7);
+CONST I3 = RANDOM_CHOICE(1, 2, 4, 8, 11, 13, 16, 17, 19, 22);
+
+int main () {
+/// END HIDDEN
+    int r;
+    r = I3 / I1;
+/// BEGIN HIDDEN
+    cout << r << endl;
+}
+/// Objectif Pédagigique : division entière.
+/// END HIDDEN
