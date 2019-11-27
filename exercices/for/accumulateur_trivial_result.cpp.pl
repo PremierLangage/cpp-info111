26bc04c451ed812ca90e12fc596bdc2d1564c19d
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-11-27 16:47:09 +0100

    ajout des exos for

---
+++ exercices/for/accumulateur_trivial_result.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_result.pl
+template=/cpp-info111/exercices_src/for/metadata.pl
+@ /cpp-info111/exercices_src/for/accumulateur_trivial_result.cpp [raw_code.cpp]
+code_file=raw_code.cpp
---
+++ exercices/for/proc_insert_code.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_code.pl
+template=/cpp-info111/exercices_src/for/metadata.pl
+@ /cpp-info111/exercices_src/for/proc_insert_code.cpp [raw_code.cpp]
+code_file=raw_code.cpp
