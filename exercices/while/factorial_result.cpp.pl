fc373d0b93d2965a92f7dd5e8f9645754666cacf
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-11-27 17:06:04 +0100

    make apres update

---
+++ exercices/while/factorial_result.cpp.pl
@@ -0,0 +1,4 @@
+template=/cpp-info111/template/wims_result.pl
+template=/cpp-info111/exercices_src/while/metadata.pl
+@ /cpp-info111/exercices_src/while/factorial_result.cpp [raw_code.cpp]
+code_file=raw_code.cpp
