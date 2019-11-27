72e9725a31eaf3d82bdfb7d80ce6d946a48110df
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-11-27 16:46:36 +0100

    make après update

--- exercices/for/for_odd.cpp.pl
+++
@@ -1,4 +0,0 @@
-template=/cpp-info111/template/wims_output.pl
-template=/cpp-info111/exercices_src/for/metadata.pl
-@ /cpp-info111/exercices_src/for/for_odd.cpp [raw_code.cpp]
-code_file=raw_code.cpp
