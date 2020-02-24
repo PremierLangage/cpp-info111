dc1205360dad87594dc6ca942c22e530d61b9cad
Hugo Mlodecki <hugo.mlodecki@u-psud.fr>
2020-02-24 15:31:21 +0100

    hmlodecki modified Yggdrasil/cpp-info111/template/wims_result.pl

--- template/wims_result.pl
+++ template/wims_result.pl
@@ -5,9 +5,10 @@ name=deviner le résultat
 typename=Deviner le résultat
 type=sandbox
 
-failure_message=Réponse incorrecte:<pre>{answer_out}</pre>La variable r contient:<pre>{solution_out}</pre>
+failure_message=Ce n'est pas la réponse attendue. La variable r contient:<pre>{solution_out}</pre>
 
 text==
 Exécuter pas à pas le programme suivant. Quelle est la valeur
 de la variable r à la fin?
 ==
+
