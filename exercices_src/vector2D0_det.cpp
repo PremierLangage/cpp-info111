7fd99ad54eef0e98a1dc17021ad68e9e26b9e3a9
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-02-25 17:31:41 +0100

    nthiéry modified Yggdrasil/cpp-info111/exercices_src/vector2D0_det.cpp

--- exercices_src/vector2D0_det.cpp
+++ exercices_src/vector2D0_det.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+CONST V22 = RANDOM_VECTOR(2, RANDOM_VECTOR, 2, RANDOM_INT, 0, 5);
 
 int main() {
 /// END HIDDEN
@@ -13,3 +14,4 @@ int main() {
 /// BEGIN HIDDEN
 }
 /// END HIDDEN
+
