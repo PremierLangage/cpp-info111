b796051ed6652788c6c7442a7be99631a34fc422
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-02-29 17:01:12 +0100

    nthiéry modified Yggdrasil/cpp-info111/exercices_src/vector2D0_basic_1.cpp

--- exercices_src/vector2D0_basic_1.cpp
+++ exercices_src/vector2D0_basic_1.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+CONST V23 = RANDOM_VECTOR(2, RANDOM_VECTOR, 3, RANDOM_INT, 0, 5);
 
 int main() {
 /// END HIDDEN
-    vector<vector <int>> tab = RANDOM_VECTOR(2, RANDOM_VECTOR, 3, RANDOM_INT, 0, 5);
+    vector<vector <int>> tab = V23;
 
     cout << tab[CI5][CI5] << endl;
 /// BEGIN HIDDEN
 }
 /// END HIDDEN
+
 
