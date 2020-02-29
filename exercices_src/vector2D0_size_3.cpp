49a609a17c11a7d81e0c3a0d18ffba289f94e74f
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-02-29 16:54:41 +0100

    nthiéry modified Yggdrasil/cpp-info111/exercices_src/vector2D0_size_3.cpp

--- exercices_src/vector2D0_size_3.cpp
+++ exercices_src/vector2D0_size_3.cpp
@@ -7,8 +7,9 @@ int main() {
 /// END HIDDEN
     vector<vector <int>> tab = RANDOM_VECTOR(2, RANDOM_VECTOR, 3, RANDOM_INT, 0, 5);
 
-    cout << tab[RANDOM_INT(2)].size() << endl;
+    cout << tab[RANDOM_INT(0,1)].size() << endl;
 /// BEGIN HIDDEN
 }
 /// END HIDDEN
+
 
