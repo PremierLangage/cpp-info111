d59f406ef93009a34a2be7ab1454a1aea9c78de6
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-02-29 23:32:28 +0100

    nthiéry modified Yggdrasil/cpp-info111/exercices_src/vector2D_loop_1.cpp

--- exercices_src/vector2D_loop_1.cpp
+++ exercices_src/vector2D_loop_1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+CONST V22 = RANDOM_VECTOR(2, RANDOM_VECTOR, 3, RANDOM_INT, 0, 5);
 
 int main() {
 /// END HIDDEN
@@ -17,3 +18,4 @@ int main() {
 /// BEGIN HIDDEN
 }
 /// END HIDDEN
+
