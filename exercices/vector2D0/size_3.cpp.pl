17017683f27a571febf3d99e86e8a06c2b59738e
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-03-01 11:35:07 +0100

    nthiéry modified Yggdrasil/cpp-info111/exercices/vector2D0/size_3.cpp.pl

--- exercices/vector2D0/size_3.cpp.pl
+++ exercices/vector2D0/size_3.cpp.pl
@@ -1,4 +1,32 @@
-template=/cpp-info111/template/wims_output.pl
-template=/cpp-info111/exercices_src/vector2D0/metadata.pl
-@ /cpp-info111/exercices_src/vector2D0_size_3.cpp [raw_code.cpp]
-code_file=raw_code.cpp
+topicname=Contrôle de révision pour l'examen d'Info 111
+title={{topicname}}
+introduction==
+Cette fiche d'exercice notée vient conclure la série de fiches de
+révisions pour l'examen d'Info 111.
+==
+@ /cpp-info111/exercices/variable/3_echange_fail1_result.cpp.pl
+@ /cpp-info111/exercices/variable/0_declaration_float_code.cpp.pl
+@ /cpp-info111/exercices/variable/5_string2_result.cpp.pl
+@ /cpp-info111/exercices/arithmetic/division_entiere_2_result.cpp.pl
+@ /cpp-info111/exercices/arithmetic/z_parentheses_result.cpp.pl
+@ /cpp-info111/exercices/io_output_expr_1.cpp
+@ /cpp-info111/exercices/io_output_basic_2.cpp
+@ /cpp-info111/exercices/while/empty.cpp.pl
+@ /cpp-info111/exercices/while/accumulateur.cpp.pl
+@ /cpp-info111/exercices/for/output.cpp.pl
+@ /cpp-info111/exercices/for/binomial_input.cpp.pl
+@ /cpp-info111/exercices/for/sum_inverse_code.cpp.pl
+@ /cpp-info111/exercices/vector1D/recherche_first_wrong.cpp.pl
+@ /cpp-info111/exercices/vector1D/recherche_last.cpp.pl
+@ /cpp-info111/exercices/vector1D/sum_input.cpp.pl
+@ /cpp-info111/exercices/function_inversion_arguments.cpp.pl
+@ /cpp-info111/exercices/function_call_3.cpp.pl
+@ /cpp-info111/exercices/if/semicolon.cpp.pl
+@ /cpp-info111/exercices/fornested_facile1.cpp.pl
+@ /cpp-info111/exercices/vector2D0_size_1.cpp.pl
+@ /cpp-info111/exercices/vector2D0_size_2.cpp.pl
+@ /cpp-info111/exercices/vector2D_loop_2.cpp.pl
+@ /cpp-info111/exercices/vector2D_2a_input.cpp.pl
+@ /cpp-info111/exercices/file_ostringstream.cpp.pl
+@ /cpp-info111/exercices/file_state_open.cpp.pl
+
