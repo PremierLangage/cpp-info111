0af832372dbf832fda8310697fb44bb2ecc94ca6
Hugo Mlodecki <hugo.mlodecki@u-psud.fr>
2020-03-04 10:49:59 +0100

    hmlodecki modified Yggdrasil/cpp-info111/template/wims_input.pl

--- template/wims_input.pl
+++ template/wims_input.pl
@@ -9,7 +9,7 @@ text==
 Quel nombre devrait saisir l'utilisateur pour que le programme C++ suivant affiche {{solution_out}}?
 ==
 
-failure_message= Ce n'est pas la réponse attendue. Le programme a affiché: <pre>{answer_out}</pre>
+failure_message= Ce n'est pas la réponse attendue.# Le programme a affiché: <pre>{answer_out}</pre>
 
 build==
 from mybuilder import build_generic, build_finalize
@@ -20,5 +20,6 @@ def build(exo):
                    [{'type':'solution', 'subtype': 'stdout', 'content': str(exo['solution_out'])}]
     return build_finalize(exo)
 ==
+
 
 
