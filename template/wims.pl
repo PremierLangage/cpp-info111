bafac7529069e9a86dc71b4bb51afbfdad0b6eca
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-08 23:13:11 +0200

    nthiéry modified Yggdrasil/cpp-info111/template/wims.pl

---
+++ template/wims.pl
@@ -0,0 +1,14 @@
+# Copyright 2018 Nicolas M. Thiéry ...
+
+author=MPI Info 111's team
+# chargement des fichiers utiles
+@ lib:/builder/build.py [builder.py]
+
+@ /cpp-info111/template/plutils.py
+@ /cpp-info111/template/graderCpp.py
+@ /cpp-info111/template/randomization.h
+@ /cpp-info111/template/builder.py [mybuilder.py]
+
+form=@ /cpp-info111/template/form/editorform.html
+
+
