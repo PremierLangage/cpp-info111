774c559b59473f7802769ac632184631986efea9
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-08 23:13:30 +0200

    nthiéry modified Yggdrasil/cpp-info111/template/wims.pl

--- template/wims.pl
+++ template/wims.pl
@@ -20,4 +20,32 @@ def build(exo):
     builder = wims.WimsOutput()
     return builder(exo)
 ==
+grader==
+import sys
+from graderCpp import grader_generic
+
+if __name__ == "__main__":
+    import json
+    with open(sys.argv[1]) as f:
+        context = json.load(f)
+    with open(sys.argv[2]) as f:
+        response = json.load(f)
+
+    context["response"] = response
+
+    result = grader_generic(context)
+
+    feedback = result['feedback']
+    if result['success']:
+        feedback = '<div id="success" class="alert alert-success feedback">'+feedback+'</div>'
+        print(100)
+    else:
+        feedback = '<div id="success" class="alert alert-danger feedback">'+feedback+'</div>'
+        print(0)
+
+    with open(sys.argv[4], 'w+') as f:
+        print(feedback, file=f)
+
+    sys.exit(0)
+==
 
