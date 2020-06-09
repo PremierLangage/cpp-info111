321b89ab5f770a9579008cc39e222b0706ee6fc9
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 07:34:07 +0200

    nthiéry modified Yggdrasil/cpp-info111/exercices/tests.pl

---
+++ exercices/tests.pl
@@ -0,0 +1,11 @@
+template=/cpp-info111/template/wims.pl
+template=/cpp-info111/exercices_src/tests/metadata.pl
+@ /cpp-info111/exercices/tests/test_code.cpp
+@ /cpp-info111/exercices/tests/test_input.cpp
+@ /cpp-info111/exercices/tests/test_multisolution_code.cpp
+@ /cpp-info111/exercices/tests/test_output.cpp
+@ /cpp-info111/exercices/tests/test_randomization.cpp
+@ /cpp-info111/exercices/tests/test_result.cpp
+@ /cpp-info111/exercices/tests/test_return.cpp
+@ /cpp-info111/exercices/tests/test_silentcode.cpp
+
