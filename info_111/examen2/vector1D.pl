7a31bb971a99a93b6e8dec3705b37a65db9c8f3d
Nicolas M. Thiéry <nthiery@users.sf.net>
2020-06-09 21:37:43 +0200

    Squelette examen2

---
+++ info_111/examen2/vector1D.pl
@@ -0,0 +1,9 @@
+template=/cpp-info111/template/wims.pl
+title=Vecteurs
+introduction=Exercice sur les vecteurs
+@ /cpp-info111/exercices_src/vector1D/recherche_first_decreasing.cpp
+@ /cpp-info111/exercices_src/vector1D/recherche_first_wrong.cpp
+@ /cpp-info111/exercices_src/vector1D/recherche_last.cpp
+@ /cpp-info111/exercices_src/vector1D/recherche_wright.cpp
+@ /cpp-info111/exercices_src/vector1D/sum_input.cpp
+@ /cpp-info111/exercices_src/vector1D/tri_code.cpp
---
+++ info_111/examen2/vector2D.pl
@@ -0,0 +1,12 @@
+template=/cpp-info111/template/wims.pl
+title=Vecteurs 2D
+introduction=Exercice sur les vecteurs 2D
+
+@ /cpp-info111/exercices/vector2D/1_input.cpp.pl
+@ /cpp-info111/exercices/vector2D/2a_input.cpp.pl
+@ /cpp-info111/exercices/vector2D/2b_input.cpp.pl
+@ /cpp-info111/exercices/vector2D/loop_0.cpp.pl
+@ /cpp-info111/exercices/vector2D/loop_1.cpp.pl
+@ /cpp-info111/exercices/vector2D/loop_2.cpp.pl
+@ /cpp-info111/exercices/vector2D/loop_3.cpp.pl
+@ /cpp-info111/exercices/vector2D/loop_4.cpp.pl
