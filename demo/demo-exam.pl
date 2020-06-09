9c788e71f26149b2cb1b7b85025e7245c477c5b5
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 09:59:05 +0200

    nthiéry modified Yggdrasil/cpp-info111/demo/demo-exam.pl

--- demo/demo-exam.pl
+++ demo/demo-exam.pl
@@ -1,12 +1,5 @@
 topicname=Démo, mode examen
 title=Mode examen
-template=/cpp-info111/template/wims.pl
+template=demo.pl
 mode=exam
-@ /cpp-info111/exercices_src/tests/test_code.cpp
-@ /cpp-info111/exercices_src/tests/test_input.cpp
-@ /cpp-info111/exercices_src/tests/test_multisolution_code.cpp
-@ /cpp-info111/exercices_src/tests/test_output.cpp
-@ /cpp-info111/exercices_src/tests/test_randomization.cpp
-@ /cpp-info111/exercices_src/tests/test_result.cpp
-@ /cpp-info111/exercices_src/tests/test_return.cpp
-@ /cpp-info111/exercices_src/tests/test_silentcode.cpp
+
