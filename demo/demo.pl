a83445af2bfdcf15f10f524f60897e2d5ec1b18a
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 10:10:31 +0200

    nthiéry modified Yggdrasil/cpp-info111/demo/demo.pl

--- demo/demo.pl
+++ demo/demo.pl
@@ -3,7 +3,7 @@ title=Mode démo
 template=/cpp-info111/template/wims.pl
 mode=demo
 settings.allow_reroll = true
-settings.reroll_threshold = 0
+settings.reroll_threshold = -1
 @ /cpp-info111/exercices_src/tests/test_code.cpp
 @ /cpp-info111/exercices_src/tests/test_input.cpp
 @ /cpp-info111/exercices_src/tests/test_multisolution_code.cpp
