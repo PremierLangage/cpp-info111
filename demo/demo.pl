d10c81346abcadc75b7cdf3be492980e3b415d8e
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 10:30:20 +0200

    nthiéry modified Yggdrasil/cpp-info111/demo/demo.pl

--- demo/demo.pl
+++ demo/demo.pl
@@ -12,3 +12,24 @@ settings.reroll_threshold = -1
 @ /cpp-info111/exercices_src/tests/test_result.cpp
 @ /cpp-info111/exercices_src/tests/test_return.cpp
 @ /cpp-info111/exercices_src/tests/test_silentcode.cpp
+form+=
+{{ hints|component}}
+==
+hints % { "cid": "hints", "selector": "c-hint" }
+hints.label = **Stucked?** Ask a hint.
+hints.confirmMessage ==
+The usage of a hint may affect your final score!
+==
+
+hints.shouldConfirm % true
+hints.confirmTitle = Are you sure you want a hint ?
+hints.confirmOkTitle = Yes
+hints.confirmNoTitle = No
+hints.items %=
+[
+    { "content": "**Hint 1**" },
+    { "content": "**Hint 2**"  },
+    { "content": "**Hint 3**", "css": "warning-state"  }
+]
+==
+
