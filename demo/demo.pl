63cd450242accbf0eae14d7d303327b4dc10eb79
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 09:49:50 +0200

    nthiéry modified Yggdrasil/cpp-info111/demo/demo.pl

--- demo/demo.pl
+++ demo/demo.pl
@@ -1,6 +1,6 @@
 topicname=Démo du moteur d'exercices cpp-info111 de PLaTon
 template=/cpp-info111/template/wims.pl
-mode=training
+mode=demo
 @ /cpp-info111/exercices_src/tests/test_code.cpp
 @ /cpp-info111/exercices_src/tests/test_input.cpp
 @ /cpp-info111/exercices_src/tests/test_multisolution_code.cpp
