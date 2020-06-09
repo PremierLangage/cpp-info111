7e8a3db5bf8a4fbea56f81d01f383c9da964ed05
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-09 07:26:46 +0200

    nthiéry modified Yggdrasil/cpp-info111/exercices/tests/test_input.cpp.pl

--- exercices/tests/test_input.cpp.pl
+++ exercices/tests/test_input.cpp.pl
@@ -1,5 +1,4 @@
 template=/cpp-info111/template/wims.pl
 template=/cpp-info111/exercices_src/tests/metadata.pl
-@ /cpp-info111/exercices_src/tests/test_input.cpp [test_input.cpp]
-code_file=test_input.cpp
+@ /cpp-info111/exercices_src/tests/test_input.cpp
 
