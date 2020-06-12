5936d65f651f61da7e70280e1c30fde5e566f5f9
Nicolas Thiéry <nicolas.thiery@u-psud.fr>
2020-06-12 11:04:02 +0200

    nthiéry modified Yggdrasil/cpp-info111/demo/demo.pl

--- demo/demo.pl
+++ demo/demo.pl
@@ -6,9 +6,10 @@ settings.allow_reroll = true
 settings.reroll_threshold = -1
 @ /cpp-info111/exercices_src/tests/test_code.cpp
 @ /cpp-info111/exercices_src/tests/test_input.cpp
-@ /cpp-info111/exercices_src/tests/test_multisolution_code.cpp
+@ /cpp-info111/exercices_src/tests/test_multisolution_silentcode.cpp
 @ /cpp-info111/exercices_src/tests/test_output.cpp
 @ /cpp-info111/exercices_src/tests/test_randomization.cpp
 @ /cpp-info111/exercices_src/tests/test_result.cpp
 @ /cpp-info111/exercices_src/tests/test_return.cpp
 @ /cpp-info111/exercices_src/tests/test_silentcode.cpp
+
